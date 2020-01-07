92491a135e287074cc8595ec00aedb586737762b
Myles Carey <seanmyles@comcast.net>
2020-01-06 19:05:30 -0800

    Updates

---
+++ precache-manifest.928584d40045e0b7598735a1be71bf62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eca5db4fdd0919a2f23fcbbc39c3cff",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "96f7a82b1bf29800214f",
+    "url": "/portfolio2/static/css/main.da8cd4c2.chunk.css"
+  },
+  {
+    "revision": "59b9ce850325628415f2",
+    "url": "/portfolio2/static/js/2.54bb1144.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/portfolio2/static/js/2.54bb1144.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96f7a82b1bf29800214f",
+    "url": "/portfolio2/static/js/main.16a960a2.chunk.js"
+  },
+  {
+    "revision": "5178ef21f4b17c1b295b",
+    "url": "/portfolio2/static/js/runtime-main.725c372b.js"
+  },
+  {
+    "revision": "b97ee1b59ef9179e01fee4ceca3d3710",
+    "url": "/portfolio2/static/media/MylesCarey.b97ee1b5.jpg"
+  },
+  {
+    "revision": "f619aada20e8cd03181d6e897acabafb",
+    "url": "/portfolio2/static/media/workspace.f619aada.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio2/precache-manifest.6343df725271fbfdec15661195755526.js"
+  "/portfolio2/precache-manifest.928584d40045e0b7598735a1be71bf62.js"
 );
 
 self.addEventListener('message', (event) => {
