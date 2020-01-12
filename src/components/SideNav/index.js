f46754b130674361361d348c60ca4d6a4e3fad51
Myles Carey <seanmyles@comcast.net>
2020-01-12 09:15:15 -0800

    adjusting logo alignment

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -7,21 +7,37 @@ class SideNav extends Component {
   render() {
     return (
       <aside id="sidebar-wrapper">
-      <div className="sidebar-brand">
-        <h4 id="logo">
-          <a href="#hero" onClick={ (e) => this.props.onClickNavLink(e)}>
-            <i id="home" className="fa fa-home"></i>Logo 
+      {/* <div className="sidebar-bran"> */}
+        <div className="my-name">
+          {/* <span className="logo-div">
+            <i id="home" className="fa fa-home"></i> 
+          </span> */}
+          {/* <span className="my-name"> */}
+          <a id="name-link" href="#hero" onClick={ (e) => this.props.onClickNavLink(e)}>
+            <div className="first-name">
+              <span className="name-cap">M</span><span className="name-back">yles</span>
+            </div>
+            <div className="last-name">
+              <span className="name-cap">C</span><span className="name-back">arey</span>
+            </div>
           </a>
-        </h4>
-        <h4>
+          {/* </span> */}
+        </div>
+        {/* <span className="brand-name">Myles Carey</span> */}
+        {/* <h4 id="logo">
+          <a href="#hero" onClick={ (e) => this.props.onClickNavLink(e)}>
+            <i id="home" className="fa fa-home"></i>Myles Carey 
+          </a>
+        </h4> */}
+        {/* <h4> */}
           {/* <a id="sidebar-close" href="#" onClick={ (e) => this.props.onClickSideNav(e)}
             ><i id="close" className="fa fa-window-close"></i>Close
           </a> */}
-          <button id="sidebar-close" onClick={ (e) => this.props.onClickSideNav(e)}
+          {/* <button id="sidebar-close" onClick={ (e) => this.props.onClickSideNav(e)}
             ><i id="close" className="fa fa-window-close fa-lg"></i>Close
-          </button>
-        </h4>
-      </div>
+          </button> */}
+        {/* </h4> */}
+      {/* </div> */}
       <nav>
         <Scrollspy className="sidebar-nav"
           items={["about", "skills", "projects","education","experience",
@@ -65,8 +81,18 @@ class SideNav extends Component {
                 <i className="fa fa-file-pdf"></i>Resume
               </a>
             </li>
+            <li className="li-sidebar-close">
+              <a href="#home" className="sidebar-close" onClick={ (e) => this.props.onClickSideNav(e)}
+                ><i id="close" className="fa fa-window-close fa-lg"></i>Close
+              </a>
+            </li>
           {/* </ul> */}
         </Scrollspy>
+        {/* <li>
+          <button id="sidebar-close" onClick={ (e) => this.props.onClickSideNav(e)}
+            ><i id="close" className="fa fa-window-close fa-lg"></i>Close
+          </button>
+        </li> */}
       </nav>
     </aside>
     )
