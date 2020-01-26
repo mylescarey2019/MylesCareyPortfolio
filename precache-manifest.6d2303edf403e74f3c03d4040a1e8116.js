44a9e951a68a266515b5df27fb3f38a87f24720c
Myles Carey <seanmyles@comcast.net>
2020-01-26 14:58:17 -0800

    Updates

--- precache-manifest.6d2303edf403e74f3c03d4040a1e8116.js
+++ precache-manifest.6d2303edf403e74f3c03d4040a1e8116.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98a57517aec73cfa4da86c50d49982fa",
+    "revision": "369b65c14d564e56e688a0217e17b9ab",
     "url": "/portfolio2/index.html"
   },
   {
-    "revision": "518bda9d2445cb8adb7b",
-    "url": "/portfolio2/static/css/main.c6891b18.chunk.css"
+    "revision": "6a786bc09d32d4505cf9",
+    "url": "/portfolio2/static/css/main.4cc66631.chunk.css"
   },
   {
     "revision": "dbc161e1fd1451d5a4d9",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio2/static/js/2.b56d99af.chunk.js.LICENSE"
   },
   {
-    "revision": "518bda9d2445cb8adb7b",
-    "url": "/portfolio2/static/js/main.b62a3660.chunk.js"
+    "revision": "6a786bc09d32d4505cf9",
+    "url": "/portfolio2/static/js/main.9688bfd1.chunk.js"
   },
   {
     "revision": "5178ef21f4b17c1b295b",
