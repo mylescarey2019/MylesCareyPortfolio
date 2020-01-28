691bbefc752d9467d08bf2de1740dc372f14075d
Myles Carey <seanmyles@comcast.net>
2020-01-27 21:08:10 -0800

    adding remainder of projects

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -8,7 +8,7 @@ const Experience = (props) => (
       <div className="experience-box box-border d-inline-block">
         <p className="experience-title">{props.company} - {props.date}</p>
         <p className="experience-role">{props.role}</p>
-        <ul>
+        <ul className="experience-list">
           <li className="text-left">{props.bullet1}</li>
           <li className="text-left">{props.bullet2}</li>
           <li className="text-left">{props.bullet3}</li>
