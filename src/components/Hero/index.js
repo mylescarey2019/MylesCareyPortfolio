eb8670cfc68e4e0a07553bf707e2343a915eda5e
Myles Carey <seanmyles@comcast.net>
2020-01-30 11:28:52 -0800

    working on hero banner message text

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 import './style.css';
 
 const Hero = (props) => (
+      <div>
       <div id="hero">
         <img className="img-fluid" src={props.heroimg} alt={"hero"}/>
+        <span className="hero-msg-1">Full Stack Web Developer / Programmer Analyst</span>
       </div>   
+      </div>
     )
 
 export default Hero;
