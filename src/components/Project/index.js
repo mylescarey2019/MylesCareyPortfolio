df16d2754a9a0249dfdf5b8d63742d7975d5470c
Myles Carey <seanmyles@comcast.net>
2020-01-30 19:56:06 -0800

    work on project responsiveness

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,14 +4,18 @@ import './style.css';
 import '../app.css';
 
 const Project = (props) => (
-      <div className="col-sm-6 col-xl-4">
+      <div className="col-sm-6 col-xl-4 project">
         <div className="project-box box-border d-inline-block my-5">
           <div className="image-box">
             <img className="project-img img-fluid" src={process.env.PUBLIC_URL + props.img} alt={props.title}></img>
           </div>
           <p className="project-title">{props.title}</p>
-          <p className="project-p2">{props.description}</p>
-          <p className="project-p3">{props.tech}</p>
+          {/* <p className="project-p2">{props.description}</p>
+          <p className="project-p3">{props.tech}</p> */}
+          <ul className="project-desc-list">
+            <li className="project-p2">{props.description}</li>
+            <li className="project-p3">{props.tech}</li>
+          </ul>
           <div className="project-links">
             <a className={`project-link ${props.noLink}`} href={props.extLink} rel="noopener noreferrer" target="_blank">
               <i className={props.extLinkIcon}></i>
