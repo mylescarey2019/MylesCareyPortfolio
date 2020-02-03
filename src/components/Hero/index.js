03c0bce579c8caf39b7cf052b16aa254a654ce8c
Myles Carey <seanmyles@comcast.net>
2020-02-03 15:59:14 -0800

    fixed scroll to top issue on smallest form factor

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,6 +5,7 @@ const Hero = (props) => (
       <div>
       <div id="hero">
         <img className="img-fluid" src={props.heroimg} alt={"hero"}/>
+        {/* <span className="hero-msg-1">Full Stack Web Developer / Programmer Analyst</span> */}
       </div>   
       </div>
     )
