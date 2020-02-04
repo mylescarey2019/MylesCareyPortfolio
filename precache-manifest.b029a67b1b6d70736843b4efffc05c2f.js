94f5dc36fefbe96ef1befce45ffa51118ca68ebb
Myles Carey <seanmyles@comcast.net>
2020-02-04 11:29:02 -0800

    Updates

---
+++ precache-manifest.b029a67b1b6d70736843b4efffc05c2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b356bf4d7c9a8ca7875118a57a62c03",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "bff7bf520cf2fd547160",
+    "url": "/Portfolio/static/css/main.70eb0efa.chunk.css"
+  },
+  {
+    "revision": "9dd32b90052f2465e6f7",
+    "url": "/Portfolio/static/js/2.20b43afa.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/Portfolio/static/js/2.20b43afa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bff7bf520cf2fd547160",
+    "url": "/Portfolio/static/js/main.d8413d2f.chunk.js"
+  },
+  {
+    "revision": "6a58a16603ebe426610b",
+    "url": "/Portfolio/static/js/runtime-main.d3ebe2ed.js"
+  },
+  {
+    "revision": "c93975e3e5fc5c6bd4d2e789ff31ae4a",
+    "url": "/Portfolio/static/media/cork-wallet.c93975e3.png"
+  },
+  {
+    "revision": "b542820569fafa072d5e924565609404",
+    "url": "/Portfolio/static/media/desktop13.b5428205.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio2/precache-manifest.556a2bf7c9c1e4689a0b03d70fa7a272.js"
+  "/Portfolio/precache-manifest.b029a67b1b6d70736843b4efffc05c2f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
